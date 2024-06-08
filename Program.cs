42e5e242718882480a8d908003bcd027f3ee4788
Berkay <155807794+Berkaypak@users.noreply.github.com>
2024-06-08 11:39:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace DICTIONARY1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,ArrayList> verbs = new Dictionary<string,ArrayList>()
+   {
+       {"go",new ArrayList{"went  " ,"gone"} } ,
+       {"speak",new ArrayList{"spoke  ","spoken" } },
+                {"take",new ArrayList{"took  ","taken"} } ,
+                {"eat",new ArrayList{"ate  ","eaten" } },
+                {"break", new ArrayList{"broke  ","broken"} },
+                {"be",new ArrayList{"was,were  ","been"} },
+                {"drink",new ArrayList{"drank  ","drunk"} },
+                {"give",new ArrayList{"gave  ","given"} },
+                {"know",new ArrayList{"knew  ","known"} },
+                {"make",new ArrayList{"made  ","made"} }
+   };
+            Console.WriteLine("              IRREGULAR VERBS                  ");
+            Console.WriteLine("      ------------------------------------           ");
+            Console.Write("VERBS -->>>  ");
+            foreach(string s in verbs.Keys)
+            {
+                Console.Write(s+"\t");
+            }
+            Console.WriteLine();
+            Console.Write("VERBS2-->>   ");
+            foreach(var v in verbs.Values)
+            {
+                Console.Write(v[0]);
+            }
+            Console.WriteLine();
+            Console.Write("VERBS3-->>   ");
+            foreach (var v in verbs.Values)
+            {
+                Console.Write(v[1]+"\t");
+            }
+        }
+        }
+    }
+
